86b7c7a43be7f4efbb44eb101345152492d77a68
levilu800b <levilu800b@gmail.com>
2021-05-03 14:47:43 +0100

    check

--- script.js
+++ script.js
@@ -10,6 +10,21 @@ let blackjackGame = {
 		div: '#dealer-box',
 		score: 0,
 	},
+	cards: [
+		'2',
+		'3',
+		'4',
+		'5',
+		'6',
+		'7',
+		'8',
+		'9',
+		'10',
+		'K',
+		'J',
+		'Q',
+		'A',
+	],
 };
 
 const YOU = blackjackGame['you'];
@@ -28,12 +43,19 @@ document.querySelector('#blackjack-deal-button').addEventListener(
 );
 
 function blackjackHit() {
-	showCard(YOU);
+	let card = randomCard();
+	console.log(card);
+	showCard(card, YOU);
 }
 
-function showCard(activePlayer) {
+function randomCard() {
+	let randomIndex = Math.floor(Math.random() * 13);
+	return blackjackGame['cards'][randomIndex];
+}
+
+function showCard(card, activePlayer) {
 	let cardImage = document.createElement('img');
-	cardImage.src = 'images/Q.png';
+	cardImage.src = `images/${card}.png`;
 	document.querySelector(activePlayer['div']).appendChild(cardImage);
 	hitSound.play();
 }
